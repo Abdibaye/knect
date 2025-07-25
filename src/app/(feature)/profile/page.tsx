b00608dd231ba77da8f009254f50f1d4318bbd16
Abdibaye <abdibaye0902@gmail.com>
2025-07-25 20:15:14 +0300

    api for for user profile

--- src/app/(feature)/profile/page.tsx
+++ src/app/(feature)/profile/page.tsx
@@ -1,12 +1,41 @@
-import ProfilePage from '@/components/profile/profile'
-import React from 'react'
+"use client";
 
-const page = () => {
+import ProfilePage from '@/components/profile/profile'
+import React, { useEffect, useState } from 'react'
+
+const Page = () => {
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<string | null>(null);
+  const [profile, setProfile] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchUserProfile = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const res = await fetch("/api/user");
+        if (!res.ok) throw new Error("Failed to fetch profile");
+        const data = await res.json();
+        setProfile(data);  
+      } catch (err: any) {
+        setError(err.message || "Failed to fetch profile");
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchUserProfile();
+  }, []);
+
+  useEffect(() => {
+    console.log(profile);
+  }, [profile]);
+
   return (
     <div>
-      <ProfilePage />
+      <ProfilePage profile={profile} />
     </div>
   )
 }
 
-export default page
+export default Page
